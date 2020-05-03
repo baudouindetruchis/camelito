9db7f37c0e49cf46b0b44a489d13f93ccfa148a4
EliseGabilly <47529648+EliseGabilly@users.noreply.github.com>
2020-05-03 19:21:04 +0200

    retrieve user data

--- Java Ressources/src/servlet/connexion.java
+++ Java Ressources/src/servlet/connexion.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,52 +38,35 @@ public class connexion extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		
-		List<User> result = new ArrayList<>();
+		String url = "jdbc:postgresql://127.0.0.1:5432/camelitoLocal";
+		String user = "postgres";
+		String psw = "123";
+		try (Connection con = DriverManager.getConnection(url, user, psw);
+                PreparedStatement pst = con.prepareStatement("SELECT * FROM public.\"User\"");
+                ResultSet rs = pst.executeQuery()) {
 
-        String SQL_SELECT = "Select * from USER";
+			List<User> result = new ArrayList<>();
+		
+            while (rs.next()) {            	
+            	int id = rs.getInt("id");
+            	String user_name = rs.getString("user_name");
+            	String mail = rs.getString("Mail");
+            	int type  = rs.getInt("Type");
+            	String password = rs.getString("Password");
+            	boolean status = rs.getBoolean("Status");
+            	
+            	User obj = new User();
+                obj.setId(id);
+                obj.setMail(mail);
+                obj.setPassword(password);
+                obj.setStatus(status);
+                obj.setType(type);
+                obj.setUser_name(user_name);
 
-        // auto close connection and preparedStatement
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5432/camelitoLocal", "postgres", "123");
-            PreparedStatement preparedStatement = conn.prepareStatement(SQL_SELECT)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-        	System.out.println(resultSet);
-        	
-        	ResultSetMetaData rsmd = resultSet.getMetaData();
-        	int columnsNumber = rsmd.getColumnCount();
-        	while (resultSet.next()) {
-            	System.out.println("a");
-        	    for (int i = 1; i <= columnsNumber; i++) {
-        	        if (i > 1) System.out.print(",  ");
-        	        String columnValue = resultSet.getString(i);
-        	        System.out.print(columnValue + " " + rsmd.getColumnName(i));
-                	System.out.println("b");
-        	    }
-        	    System.out.println("");
-        	}
-        	
-//            while (resultSet.next()) {
-//            	int id = resultSet.getInt("id");
-//            	String user_name = resultSet.getString("user_name");
-//            	String mail = resultSet.getString("Mail");
-//            	int type  = resultSet.getInt("Type");
-//            	String password = resultSet.getString("Password");
-//            	boolean status = resultSet.getBoolean("Status");
-//            	
-//            	User obj = new User();
-//                obj.setId(id);
-//                obj.setMail(mail);
-//                obj.setPassword(password);
-//                obj.setStatus(status);
-//                obj.setType(type);
-//                obj.setUser_name(user_name);
-//
-//                result.add(obj);
-//
-//            }
-//            result.forEach(x -> System.out.println(x));
+                result.add(obj);
+
+            }
+            result.forEach(x -> System.out.println(x.toString()));
 
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
