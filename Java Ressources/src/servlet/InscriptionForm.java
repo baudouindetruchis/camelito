0fb2ddb9a7934e4a5afa451aa8a8974aef95964e
EliseGabilly <47529648+EliseGabilly@users.noreply.github.com>
2020-05-05 13:27:56 +0200

    use autogenerated id and comment

--- Java Ressources/src/servlet/InscriptionForm.java
+++ Java Ressources/src/servlet/InscriptionForm.java
@@ -24,108 +24,106 @@ import obj.User;
 @WebServlet("/InscriptionForm")
 public class InscriptionForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InscriptionForm() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public InscriptionForm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//get values from the form request
 		String lastname = request.getParameter("lastname");
 		String firstname = request.getParameter("firstname");
 		String year = request.getParameter("year");
-		
-		
+
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		String secondPassword = request.getParameter("secondPassword");
-		
+
 		String pseudo = request.getParameter("pseudo");
-		int id = 0;
 		String categorie = request.getParameter("categorie");
-		int type=0;
-		switch(categorie) {
-			case "Client" : type = 1;
-			case "Asso" : type = 2;
-			case "Commercant" : type = 3;
-		
+		int type = 0;
+		switch (categorie) {
+		case "Client":
+			type = 1;
+		case "Asso":
+			type = 2;
+		case "Commercant":
+			type = 3;
+
 		}
-		//Boolean status 
-		
-		
-		
+
+		//connect to the bdd
 		String url = "jdbc:postgresql://127.0.0.1:5432/camelitoLocal";
 		String user = "postgres";
 		String psw = "123";
 		String page = "./view/index.jsp";
 
-        HttpSession session=request.getSession();
-        
-        try (Connection con = DriverManager.getConnection(url, user, psw))
-        {
-        	PreparedStatement getLastID = con.prepareStatement("SELECT id FROM public.users ");
-        	
-        	ResultSet rsID = getLastID.executeQuery();
-							
-	        while (rsID.next()) {            	
-	            if(rsID.isLast()) {
-	            	 id= rsID.getInt("id");
-	            	 id++;
-	            }
-
-	        }
-	        System.out.println(password);
-	        System.out.println(secondPassword);
-	        System.out.println(password==secondPassword);
-	        if(password.contentEquals(secondPassword)) {
-	        	PreparedStatement addUser = con.prepareStatement("INSERT INTO public.users VALUES('"+
-		            	id+"','"+pseudo+"','"+email+"','"+type+"','"+password+"'," + true+")");
-		       addUser.execute();
-		        
-		       User obj = new User();
-               obj.setId(id);
-               obj.setMail(email);
-               obj.setPassword(password);
-               //obj.setStatus(status);
-               obj.setType(type);
-               obj.setUser_name(pseudo);
-		       
-               session.setAttribute("user",obj);  
-	            session.setAttribute("user_id",obj.getId());  
-	            session.setAttribute("mail",obj.getMail());
-	            session.setAttribute("userName",obj.getUser_name());
-	            
-	            session.setAttribute("msg", "Hello world"); 
-		       page = "./view/profil.jsp";
-	        }else {
-	        	System.out.println("erreur sur les mdp");
-	        }
-	        
-	        
+		HttpSession session = request.getSession();
+		try (Connection con = DriverManager.getConnection(url, user, psw)) {
 			
-            
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			//if the two password match
+			System.out.println(password);
+			System.out.println(secondPassword);
+			System.out.println(password == secondPassword);
+			if (password.contentEquals(secondPassword)) { 
+				//add a user to the bdd based on form value
+				PreparedStatement addUser = con.prepareStatement("INSERT INTO public.users(user_name, mail, type, password) "
+						+ "VALUES('"+ pseudo + "','" + email + "'," + type + ",'" + password + "')");
+				addUser.execute();
+				
+				//get auto generated id
+				PreparedStatement getId = con.prepareStatement("SELECT MAX(ID) AS id FROM public.users LIMIT 1");
+				ResultSet rs = getId.executeQuery();
+				rs.next();
+				int id = rs.getInt("id");
+				
+				// create the object user
+				User obj = new User();
+				obj.setId(id);
+				obj.setMail(email);
+				obj.setPassword(password);
+				obj.setType(type);
+				obj.setUser_name(pseudo);
 
-		//if request is not from HttpServletRequest, you should do a typecast before
-		  //save message in session
+				//add all value to the 
+				session.setAttribute("user", obj);
+				session.setAttribute("user_id", obj.getId());
+				session.setAttribute("mail", obj.getMail());
+				session.setAttribute("userName", obj.getUser_name());
+
+				//finaly load the profil page
+				page = "./view/profil.jsp";
+				
+			} else {
+				System.out.println("erreur sur les mdp");
+			}
+		} catch (SQLException e) {
+			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// if request is not from HttpServletRequest, you should do a typecast before
+		// save message in session
 		response.sendRedirect(page);
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
