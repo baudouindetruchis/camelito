70cadf5953f49eac9a7f66b89dbe470e41b520e8
EliseGabilly <47529648+EliseGabilly@users.noreply.github.com>
2020-05-19 21:25:20 +0200

    Display current quantity on shopping page

--- Java Ressources/src/servlet/ShoppingClick.java
+++ Java Ressources/src/servlet/ShoppingClick.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import JavaFunction.ShoppingListFunctions;
 
@@ -31,6 +32,8 @@ public class ShoppingClick extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ShoppingListFunctions.modifQuantity(request);
+		HttpSession session = request.getSession();
+		ShoppingListFunctions.setArticleList(session);
 		response.setStatus(HttpServletResponse.SC_NO_CONTENT);
 	}
 
